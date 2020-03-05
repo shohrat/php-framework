1cf2a935a99ac76244b36bddc318a4148325166d
shohrat <shohrat84@gmail.com>
2020-03-05 20:58:31 +0430

    user auth

--- app/controllers/Pages.php
+++ app/controllers/Pages.php
@@ -1,26 +1,24 @@
-<?php 
-
-class Pages extends Controller{
-
+<?php
+  class Pages extends Controller {
     public function __construct(){
-
+     
     }
-
+    
     public function index(){
-        $data = [
-            'title' => 'Welcome',
-            'description' => 'Simple social Network'
-        ];
-        $this->view('pages/index', $data);
+      $data = [
+        'title' => 'SharePosts',
+        'description' => 'Simple social network built on the TraversyMVC PHP framework'
+      ];
+     
+      $this->view('pages/index', $data);
     }
 
     public function about(){
-        $data = [
-            'title' => 'About Us',
-            'description' => 'App to share posts with other users'
-        ];
-        $this->view('pages/about', $data);
+      $data = [
+        'title' => 'About Us',
+        'description' => 'App to share posts with other users'
+      ];
+
+      $this->view('pages/about', $data);
     }
-
-
-}
+  }
--- app/libraries/controller.php
+++ app/libraries/controller.php
@@ -1,34 +1,26 @@
 <?php
-/*
- * Created by Bayramklychov Shohrad
- * Base Controller
- * Loads the modules and views
-*/
-class Controller {
-
-    //Load model
+  /*
+   * Base Controller
+   * Loads the models and views
+   */
+  class Controller {
+    // Load model
     public function model($model){
-    // Require model file    
-        require_once '../app/models/' . $model . '.php';
+      // Require model file
+      require_once '../app/models/' . $model . '.php';
 
-    // Instatiate model
-        return  new $model();
-   
+      // Instatiate model
+      return new $model();
     }
 
     // Load view
     public function view($view, $data = []){
-        // Check for view file
-        if(file_exists('../app/views/'. $view . '.php')){
-            require_once '../app/views/' . $view . '.php';
-        }
-        else
-        {
-            //View does not exist
-            die('View does not exist');
-        }
-
+      // Check for view file
+      if(file_exists('../app/views/' . $view . '.php')){
+        require_once '../app/views/' . $view . '.php';
+      } else {
+        // View does not exist
+        die('View does not exist');
+      }
     }
-
-}
-
+  }
